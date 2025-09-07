5716fa4ef35688916c0ced52705af0d7b0318f84
మోహన్ కృష్ణ సాయి కొప్పురావూరి <42874525+mohankoppuravuri@users.noreply.github.com>
2025-09-07 22:38:26 +0530

    Log User Agent

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
+    console.info("User Agent:", req.headers.get("user-agent"));
     return serveFile(req, "./PXL_20250612_071957188.jpg");
 });
